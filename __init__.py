0bf7b4516059eb3e29a760247ea5fa61a977f4bd
willshua <wills.hua96@gmail.com>
2021-05-11 19:56:44 +0800

    一点点调整

--- __init__.py
+++ __init__.py
@@ -5,9 +5,8 @@
 A simple back testing tool
 """
 
-__all__ = []
+__all__ = ["returns", "sharpe"]
 __author__ = "Wills Hua"
 __version__ = "0.0.1"
 
-from analyzer import returns, shaperatio
-
+from analyzer import returns, sharpe
