c2e5b0a0faa18521345c6c064493dbed76203841
willshua <wills.hua96@gmail.com>
2021-05-11 21:28:50 +0800

    一点点小调整

--- __init__.py
+++ __init__.py
@@ -6,7 +6,8 @@ A simple back testing tool
 """
 
 __all__ = ["returns", "sharpe"]
-__author__ = "Wills Hua"
 __version__ = "0.0.1"
+__author__ = "Wills Hua"
+
 
 from analyzer import returns, sharpe
