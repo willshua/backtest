3c2e2e070456291b7f9dc92bcc963e798ce488a9
willshua <wills.hua96@gmail.com>
2021-05-13 23:44:59 +0800

    一点点小调整

--- __init__.py
+++ __init__.py
@@ -8,9 +8,10 @@ A simple back testing module
 __all__ = ["drawdown",
            "returns",
            "sharpe",
+           "trade",trade
            ]
 __version__ = "0.0.1"
 __author__ = "Wills Hua <wills.hua96@gmail.com>"
 
 
-from analyzer import drawdown, returns, sharpe
+from analyzer import drawdown, returns, sharpe, trade
