fe9fc48b15d4394093c08af29d2da82294d785a6
willshua <wills.hua96@gmail.com>
2021-05-28 23:15:28 +0800

    一点点小调整

--- __init__.py
+++ __init__.py
@@ -1,16 +1,18 @@
 """
 backtest
-----------------------------
+---------------------------------
 
-A simple back testing module
+一个不知道会是啥样的量化回测工具, 
+还没想好取啥名, 更没想好怎么写.
 """
 
-__all__ = ["drawdown",
-           "returns",
-           "sharpe",
-           "trade",trade
-           ]
-__version__ = "0.0.1"
+__all__ = [
+    "drawdown",
+    "returns",
+    "sharpe",
+    "trade",
+]
+__version__ = "0.0.0"
 __author__ = "Wills Hua <wills.hua96@gmail.com>"
 
 
